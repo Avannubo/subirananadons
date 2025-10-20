0992c6cd69f6cf012edb3e702fadd0bbf909cfae
Arjun Singh <arjun.singh@ad.avancemgrup.com>
2025-10-20 09:26:05 +0200

    1

--- src/components/cart/ModalTPV.jsx
+++ src/components/cart/ModalTPV.jsx
@@ -200,6 +200,7 @@ export default function ModalTPV({ isOpen, onClose, orderData }) {
                 "DS_MERCHANT_ORDER": merchantOrder,
                 "DS_MERCHANT_TERMINAL": "2",
                 "DS_MERCHANT_TRANSACTIONTYPE": "0",
+                // "DS_MERCHANT_URL": `${window.location.origin}`,
                 "DS_MERCHANT_URLOK": `${window.location.origin}/cart/order/success`,
                 "DS_MERCHANT_URLKO": `${window.location.origin}/cart/order/failed`
             };
